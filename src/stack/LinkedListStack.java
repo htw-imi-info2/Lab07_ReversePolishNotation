bbd77afc50565abc13a3223c053b4e73863cbd98
B Kleinen <kleinen@infrastructure.de>
2017-05-19 15:20:36 +0200

    added toString

--- src/stack/LinkedListStack.java
+++ src/stack/LinkedListStack.java
@@ -1,29 +1,41 @@
 package stack;
 
 public class LinkedListStack<E> implements Stack<E> {
-
+	class Node<F>{
+		F data;
+		Node<F> next;
+		public Node(F data){this.data = data;}
+	}
+	Node<E> top;
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return top == null;
 	}
 
 	@Override
 	public E top() throws Underflow {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) throw new Underflow();
+		return top.data;
 	}
 
 	@Override
 	public void push(E element) {
-		// TODO Auto-generated method stub
-		
+		Node<E> node = new Node(element);
+		node.next = top;
+		top = node;
 	}
 
 	@Override
 	public void pop() throws Underflow {
-		// TODO Auto-generated method stub
-		
+		if (isEmpty()) throw new Underflow();
+		top = top.next;	
 	}
-
+	@Override
+	public String toString(){
+		if (isEmpty()) return "";
+		String result = top.data.toString();
+		for(Node<E> c = top.next; c != null; c = c.next)	
+			result += ", " + c.data.toString();
+		return result;
+	}
 }
